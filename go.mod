93f912404384ceacf3859ca154d46ef6ab878cb8
Benjamin Pollack <benjamin@bitquabit.com>
2026-01-12 09:31:26 -0500

    sketch of the commands

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module git.sr.ht/~bmp/jj-domino
 
 go 1.25.5
+
+require github.com/alecthomas/kong v1.13.0
--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/alecthomas/kong"
+)
+
+type cli struct {
+	Submit submitCmd `cmd:"" help:"Submit a review stack"`
+	Doctor doctorCmd `cmd:"" help:"Verify auth and config settings"`
+}
+
+type submitCmd struct {
+	Draft        bool   `short:"d" help:"Submit PR as draft"`
+	TemplatePath string `short:"t" help:"Template path"`
+}
+
+type doctorCmd struct{}
+
+func (c *submitCmd) Run() error {
+	return nil
+}
+
+func (c *doctorCmd) Run() error {
+	return nil
+}
 
 func main() {
-	fmt.Println("yo")
+	ctx := kong.Parse(&cli{}, kong.UsageOnError())
+	if err := ctx.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
